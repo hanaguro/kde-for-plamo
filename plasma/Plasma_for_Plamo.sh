7be8a9cd853aaf27a8bdccf8c780653758835a9d
Takahiro Yoshizawa <kuro@takahiro.org>
2025-01-01 06:53:00 +0900

    6.2.4にアップデート

--- plasma/Plasma_for_Plamo.sh
+++ plasma/Plasma_for_Plamo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-VERS=5.27.11
-SITE=https://download.kde.org/stable/plasma/5.27.11/
+VERS=6.2.4
+SITE=https://download.kde.org/stable/plasma/$VERS/
 
 while read -r col1 col2 _; do
         if [[ "${col1:0:1}" != "#" ]]; then
@@ -23,7 +23,7 @@ do
         tar xvf $i
         version=$(echo "$i" | sed 's/\(.*\)\.tar\.xz/\1/')
         make_PlamoBuild.py ${version} -u ${SITE}$i
-        sed -i 's/OPT_CONFIG="-DCMAKE_BUILD_TYPE=Release"/OPT_CONFIG="-DCMAKE_BUILD_TYPE=Release -DKDE_INSTALL_LIBDIR=lib"/' PlamoBuild.${version}
+        sed -i 's/OPT_CONFIG="-DCMAKE_BUILD_TYPE=Release"/OPT_CONFIG="-DCMAKE_BUILD_TYPE=Release -DKDE_INSTALL_LIBDIR=lib -DBUILD_QT5=OFF -DBUILD_TESTING=OFF"/' PlamoBuild.${version}
         ./PlamoBuild.${version}
         updatepkg -f *.tzst
         mv *.tzst ..
